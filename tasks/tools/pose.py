2a8a0e01673a14e5ec91728b77d70d8b552c43db
winstr <seunghyeon.me@gmail.com>
2023-11-12 15:28:36 +0000

    refactoring

--- tasks/tools/pose.py
+++ tasks/tools/pose.py
@@ -6,39 +6,15 @@ Author: Seunghyeon Kim (winstr)
 -------------------------------
 ...
 """
+from typing import Iterable
 
-
-from typing import Any
-
+import cv2
 import numpy as np
 import torch
 from torchvision import transforms
 
 from yolov7.utils.general import non_max_suppression_kpt
 from yolov7.utils.plots import output_to_keypoint
-
-
-def get_device(device_type: str=None) -> torch.device:
-    if device_type is None:
-        device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
-    device = torch.device(device)
-    return device
-
-
-def load_model(weight_path: str, device: torch.device) -> Any:
-    model = torch.load(weight_path, map_location=device)["model"]
-    model.float().eval()
-    if torch.cuda.is_available():
-        model.half().to(device)
-    return model
-
-
-def preprocess(img: np.ndarray, device: torch.device) -> torch.Tensor:
-    tensor = transforms.ToTensor()(img)
-    tensor = torch.tensor(np.array([tensor.numpy()]))
-    if torch.cuda.is_available():
-        tensor = tensor.half().to(device)
-    return tensor
 
 
 class PoseEstimator():
@@ -49,17 +25,38 @@ class PoseEstimator():
                  cnf_th: float=0.25,
                  iou_th: float=0.65,):
 
-        self._device = get_device(device_type)
-        self._model  = load_model(weight_path, self._device)
+        if device_type is None:
+            device_type = "cuda" if torch.cuda.is_available() else "cpu"
+        self._device = torch.device(device_type)
+
+        self._model = torch.load(weight_path, self._device)["model"]
+        self._model.float().eval()
+        if torch.cuda.is_available():
+            self._model.half().to(self._device)
+
         self._stride = stride
         self._cnf_th = cnf_th
         self._iou_th = iou_th
 
-    def estimate(self, img: np.ndarray):
-        tensor = preprocess(img, self._device)
+    def _resize(self, img: np.ndarray) -> np.ndarray:
+        def fit(x):
+            if x % self._stride:
+                x = x - (x % self._stride) + self._stride
+            return x
+        src_h, src_w = img.shape[:2]  # height, width
+        dst_h, dst_w = fit(src_h), fit(src_w)
+        return cv2.resize(img, dsize=(dst_w, dst_h))
 
+    def _to_tensor(self, img: np.ndarray) -> torch.Tensor:
+        tsr = transforms.ToTensor()(img)
+        tsr = torch.tensor(np.array([tsr.numpy()]))
+        if torch.cuda.is_available():
+            tsr = tsr.half().to(self._device)
+        return tsr
+
+    def _estimate(self, tsr: torch.Tensor) -> np.ndarray:
         with torch.no_grad():
-            preds, _ = self._model(tensor)
+            preds, _ = self._model(tsr)
         preds = non_max_suppression_kpt(
             prediction=preds,
             conf_thres=self._cnf_th,
@@ -67,19 +64,28 @@ class PoseEstimator():
             nc=self._model.yaml["nc"],
             kpt_label=True,)
         preds = output_to_keypoint(preds)
+        return preds
+
+    def _to_list(self, preds: np.ndarray) -> Iterable:
+        def cxcywh2xyxy(cx, cy, w, h):
+            x_min = int(cx - w / 2)
+            x_max = int(cx + w / 2)
+            y_min = int(cy - h / 2)
+            y_max = int(cy + h / 2)
+            return x_min, y_min, x_max, y_max
 
         poses = []
         for pred in preds:
-            x, y, w, h, conf = preds[2:7]
-
-            x_min = int(x - w / 2)
-            x_max = int(x + w / 2)
-            y_min = int(y - h / 2)
-            y_max = int(y + h / 2)
-
-            kpts = pred[7:]
-
-            pose = [x_min, y_min, x_max, y_max, conf,] + kpts.tolist()
+            cx, cy, w, h, box_conf = pred[2:7]
+            x0, y0, x1, y1 = cxcywh2xyxy(cx, cy, w, h)
+            kpts = pred[7:].tolist()
+            pose = x0, y0, x1, y1, box_conf, kpts
             poses.append(pose)
+        return poses
 
+    def estimate_pose(self, img: np.ndarray):
+        img = self._resize(img)
+        tsr = self._to_tensor(img)
+        preds = self._estimate(tsr)
+        poses = self._to_list(preds)
         return poses
