c912502a4d5dc0194a6c7c5348ca045f2924ac78
winstr <seunghyeon.me@gmail.com>
2023-11-12 05:26:34 +0000

    added function: estimate(...)

--- tasks/src/pose.py
+++ tasks/src/pose.py
@@ -7,26 +7,25 @@ Author: Seunghyeon Kim (winstr)
 ...
 """
 
-'''
-import os
+
+from typing import Any
 
 import numpy as np
 import torch
 from torchvision import transforms
 
+from yolov7.utils.general import non_max_suppression_kpt
+from yolov7.utils.plots import output_to_keypoint
+
 
 def get_device(device_type: str=None) -> torch.device:
     if device_type is None:
         device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
-
     device = torch.device(device)
     return device
 
 
-def load_model(weight_path: str, device: torch.device) -> torch.nn.Module:
-    if not os.path.isfile(weight_path):
-        raise FileNotFoundError(weight_path)
-
+def load_model(weight_path: str, device: torch.device) -> Any:
     model = torch.load(weight_path, map_location=device)["model"]
     model.float().eval()
     if torch.cuda.is_available():
@@ -34,9 +33,12 @@ def load_model(weight_path: str, device: torch.device) -> torch.nn.Module:
     return model
 
 
-def transform(img: np.ndarray) -> torch.tensor:
-    #tensor = transforms.
-    pass
+def preprocess(img: np.ndarray, device: torch.device) -> torch.Tensor:
+    tensor = transforms.ToTensor()(img)
+    tensor = torch.tensor(np.array([tensor.numpy()]))
+    if torch.cuda.is_available():
+        tensor = tensor.half().to(device)
+    return tensor
 
 
 class PoseEstimator():
@@ -48,14 +50,36 @@ class PoseEstimator():
                  iou_th: float=0.65,):
 
         self._device = get_device(device_type)
-        self._model = load_model(weight_path, self._device)
+        self._model  = load_model(weight_path, self._device)
         self._stride = stride
         self._cnf_th = cnf_th
         self._iou_th = iou_th
 
-    def transform(self, img: np.ndarray) -> torch.tensor:
-        tensor = 1
-
     def estimate(self, img: np.ndarray):
-        pass
-'''
+        tensor = preprocess(img, self._device)
+
+        with torch.no_grad():
+            preds, _ = self._model(tensor)
+        preds = non_max_suppression_kpt(
+            prediction=preds,
+            conf_thres=self._cnf_th,
+            iou_thres=self._iou_th,
+            nc=self._model.yaml["nc"],
+            kpt_label=True,)
+        preds = output_to_keypoint(preds)
+
+        poses = []
+        for pred in preds:
+            x, y, w, h, conf = preds[2:7]
+
+            x_min = int(x - w / 2)
+            x_max = int(x + w / 2)
+            y_min = int(y - h / 2)
+            y_max = int(y + h / 2)
+
+            kpts = pred[7:]
+
+            pose = [x_min, y_min, x_max, y_max, conf,] + kpts.tolist()
+            poses.append(pose)
+
+        return poses
