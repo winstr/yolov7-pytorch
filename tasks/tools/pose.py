06cfbd59f68d7e664ff0bc8596792666d335d7b5
winstr <seunghyeon.me@gmail.com>
2023-11-13 08:47:19 +0000

    added comments

--- tasks/tools/pose.py
+++ tasks/tools/pose.py
@@ -2,36 +2,58 @@
 -------------------------------
 Author: Seunghyeon Kim (winstr)
 -------------------------------
-NODES = {0: "nose", 1: "L_eye", 2: "R_eye", 3: "L_ear", 4: "R_ear",
-         5: "L_shoulder", 6: "R_shoulder", 7: "L_elbow", 8: "R_elbow",
-         9: "L_wrist", 10: "R_wrist", 11: "L_hip", 12: "R_hip", 13: "L_knee",
-         14: "R_knee", 15: "L_ankle", 16: "R_ankle"}
-COLOR = ((242, 117,  26), (106,   0, 167), (143,  13, 163), (176,  42, 143),
-         (202,  70,  12), (224, 100,  97), (241, 130,  76), (252, 166,  53),
-         (252, 204,  37), ( 64,  67, 135), ( 52,  94, 141), ( 41, 120, 142),
-         ( 32, 143, 140), ( 34, 167, 132), ( 66, 190, 113), (121, 209,  81),
-         (186, 222,  39))
--------------------------------
 """
-# ↓ Temporary
+# ↓ Temporary path
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).absolute().parents[2]/"yolov7"))
-# ↑ Temporary
+# ↑ Temporary path
 from typing import Tuple
 
 import cv2
 import numpy as np
 import torch
 from torchvision import transforms
-
 from yolov7.utils.general import non_max_suppression_kpt
 from yolov7.utils.plots import output_to_keypoint
 
+""" NOTE
+KEYPOINT = {0: "nose",
+            1: "L_eye",
+            2: "R_eye",
+            3: "L_ear",
+            4: "R_ear",
+            5: "L_shoulder",
+            6: "R_shoulder",
+            7: "L_elbow",
+            8: "R_elbow",
+            9: "L_wrist",
+            10: "R_wrist",
+            11: "L_hip",
+            12: "R_hip",
+            13: "L_knee",
+            14: "R_knee",
+            15: "L_ankle",
+            16: "R_ankle",} """
 
-EDGES = {0: (1, 2), 1: (3,), 2: (4,), 3: (), 4: (), 5: (6, 7, 11),
-         6: (8, 12), 7: (9,), 8: (10,), 9: (), 10: (), 11: (12, 13),
-         12: (14,), 13: (15,), 14: (16,), 15: (), 16: ()}
+# Keypoint Connection Map
+EDGES = {0: (1, 2),     # Keypoint 0 is connected to keypoints 1 and 2.
+         1: (3,),       # Keypoint 1 is connected to kepoint 3.
+         2: (4,),       # Keypoint 2 is connected to kepoint 4.
+         3: (),         # Keypoint 3 has no outgoing connections.
+         4: (),         # Keypoint 4 has no outgoing connections.
+         5: (6, 7, 11), # Keypoint 5 is connected to keypoints 6, 7, and 11.
+         6: (8, 12),    # Keypoint 6 is connected to keypoints 8 and 12.
+         7: (9,),       # Keypoint 7 is connected to kepoint 9.
+         8: (10,),      # Keypoint 8 is connected to kepoint 10.
+         9: (),         # Keypoint 9 has no outgoing connections.
+         10: (),        # Keypoint 10 has no outgoing connections.
+         11: (12, 13),  # Keypoint 11 is connected to keypoints 12 and 13.
+         12: (14,),     # Keypoint 12 is connected to kepoint 14.
+         13: (15,),     # Keypoint 13 is connected to kepoint 15.
+         14: (16,),     # Keypoint 14 is connected to kepoint 16.
+         15: (),        # Keypoint 15 has no outgoing connections.
+         16: ()}        # Keypoint 16 has no outgoing connections.
 
 
 def xywh_to_xyxy(xywh: np.ndarray) -> np.ndarray:
@@ -41,28 +63,22 @@ def xywh_to_xyxy(xywh: np.ndarray) -> np.ndarray:
     return np.array([x0, y0, x1, y1])
 
 
+def draw_bbox(img: np.ndarray, pred: np.ndarray, color:Tuple[int, int, int]):
+    x0, y0, x1, y1 = xywh_to_xyxy(pred[:4]).astype(int)
+    cv2.rectangle(img, (x0, y0), (x1, y1), color, 1)
+
+
+def draw_pose(img: np.ndarray, pred: np.ndarray, color:Tuple[int, int, int]):
+    keypoints = pred[5:].reshape(-1, 3)[:, :2].astype(int)
+    for i in range(len(keypoints)):
+        start_keypoint = keypoints[i].tolist()
+        cv2.circle(img, start_keypoint, 2, color, -1)
+        for j in EDGES[i]:
+            end_keypoints = keypoints[j].tolist()
+            cv2.line(img, start_keypoint, end_keypoints, color, 1)
+
+
 class PoseEstimator():
-
-    @staticmethod
-    def visualization(img: np.ndarray, preds: np.ndarray):
-
-        def draw_bbox(pred):
-            x0, y0, x1, y1 = xywh_to_xyxy(pred[:4]).astype(int)
-            cv2.rectangle(img, (x0, y0), (x1, y1), (0, 255, 0), 1)
-
-        def draw_pose(pred):
-            kpts = pred[5:].reshape(-1, 3)[:, :2].astype(int)
-            for i in range(len(kpts)):
-                stt_kpt = kpts[i].tolist()
-                cv2.circle(img, stt_kpt, 2, (0, 255, 0), -1)
-                for j in EDGES[i]:
-                    end_kpt = kpts[j].tolist()
-                    cv2.line(img, stt_kpt, end_kpt, (0, 255, 0), 1)
-
-        for pred in preds:
-            draw_bbox(pred)
-            draw_pose(pred)
-
     def __init__(self,
                  weight_path: str,       # .pt file path
                  device_type: str=None,  # e.g. "cpu", "cuda", etc.
