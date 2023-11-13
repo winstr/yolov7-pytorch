6d55db00217b86b0fc37b3948dc83f161c999345
winstr <seunghyeon.me@gmail.com>
2023-11-13 08:36:27 +0000

    added: visualize_pose(...) and main(...)

--- tasks/pose_estimation.py
+++ tasks/pose_estimation.py
@@ -1,32 +1,74 @@
+"""
+-------------------------------
+Author: Seunghyeon Kim (winstr)
+-------------------------------
+"""
 import os
+import traceback
+from typing import Tuple
 
 import cv2
 
 from tools.pose import PoseEstimator
+from tools.pose import draw_bbox, draw_pose
 
 
 WEIGHTS_DIR = "./weights"
 YOLOPOSE_PT = os.path.join(WEIGHTS_DIR, "yolov7-w6-pose.pt")
 
 
-def download_weight():
+def download_weight_file():
+    import wget
+
     os.makedirs(WEIGHTS_DIR, exist_ok=True)
+    url = "https://github.com/WongKinYiu/yolov7/"\
+        + "releases/download/v0.1/yolov7-w6-pose.pt"
+    wget.download(url, WEIGHTS_DIR)
+    print(" OK.")
+
+
+def visualize_pose(cap: cv2.VideoCapture,
+                   estimator: PoseEstimator,
+                   color: Tuple[int, int, int]=(0, 255, 0)):
+    while True:
+        # get frame
+        ret, frame = cap.read()
+        assert ret, "frame end."
+        frame = cv2.resize(frame, dsize=(960, 540))
+        # esimate poses
+        frame, preds = estimator.estimate_pose(frame)
+        # visualize poses
+        for pred in preds:
+            draw_bbox(frame, pred, color)
+            draw_pose(frame, pred, color)
+        # show result
+        cv2.imshow("result", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+
+def main(video_source: str):
+    # Check if weight file(.pt) exists
     if not os.path.isfile(YOLOPOSE_PT):
-        import wget
-        url = "https://github.com/WongKinYiu/yolov7/"\
-            + "releases/download/v0.1/yolov7-w6-pose.pt"
-        wget.download(url, WEIGHTS_DIR)
-        print(" OK.")
+        download_weight_file()
+
+    # Prepare video capture
+    cap = cv2.VideoCapture(video_source)
+    assert cap.isOpened(), "cap is not openend."
+
+    # Prepare pose estimator
+    estimator = PoseEstimator(YOLOPOSE_PT)
+
+    # Do pose estimation and visualization
+    try:
+        visualize_pose(cap, estimator)
+    except:
+        traceback.print_exc()
+    finally:
+        cv2.destroyAllWindows()
+        cap.release()
 
 
 if __name__ == "__main__":
-    download_weight()
-    img = cv2.imread("running.png")
-    estimator = PoseEstimator(YOLOPOSE_PT)
-
-    img_, preds = estimator.estimate_pose(img)
-    PoseEstimator.visualization(img_, preds)
-
-    cv2.imshow("res", img_)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    video_source = "pedestrians.mp4"
+    main(video_source)
